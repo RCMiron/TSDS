310c0541a1d1cddf69081b52f4614d3a4876daa0
Radu Miron <radu.miron@zenitech.co.uk>
2018-03-13 15:27:45 +0200

    setCompareField bug fix

--- src/binary-search-tree/binary-search-tree.spec.ts
+++ src/binary-search-tree/binary-search-tree.spec.ts
@@ -27,38 +27,40 @@ describe('BinarySearchTree: setCompareField', () => {
     });
 });
 
+
 describe('BinarySearchTree: insert by condition or compareField', () => {
     it('should insert by initially set condition or compareField', () => {
         bst = new BinarySearchTree({idx: 2, score: 40});
         const newNode: TestNode = {idx: 3, score: 45};
+        const newNode2: TestNode = {idx: 3, score: 20};
+        const newNode3: TestNode = {idx: 3, score: 30};
+        const newNode4: TestNode = {idx: 3, score: 15};
         const condition = (value: TestNode) => {
             return value.score > 50;
         };
-        bst.setCompareField('idx');
-        expect(bst.compareField).to.be.equal('idx');
+        bst.setCompareField('score');
+        expect(bst.compareField).to.be.equal('score');
+
         bst.insert(newNode);
         expect(bst.right.value).to.be.equal(newNode);
-
-        bst.setInsertCondition(condition);
-        expect(bst.compareField).to.be.equal(null);
-        expect(bst.condition).to.be.equal(condition);
-
-        bst.insert(newNode);
-        expect(bst.left.value).to.be.equal(newNode);
+        bst.insert(newNode2);
+        expect(bst.left.value).to.be.equal(newNode2);
+        bst.insert(newNode3);
+        expect(bst.left.right.value).to.be.equal(newNode3);
     });
 });
-
+/*
 describe('BinarySearchTree: traverseDepth', () => {
     it('should traverse tree least to greatest', () => {
         const tree: BinarySearchTree<number> = new BinarySearchTree<number>(10);
-        const numbers = [100, 5];
+        const numbers = [100, 5, 66, 72, 55, 3, -50];
         let result = [];
         numbers.forEach((number) => {
             tree.insert(number);
         });
         tree.traverseDepth((value) => {
-            result.push(value);
+            console.log(value);
         }, 'inOrder');
-console.log(result)
+
     })
-});
+});*/
--- src/binary-search-tree/binary-search-tree.ts
+++ src/binary-search-tree/binary-search-tree.ts
@@ -1,18 +1,19 @@
-import {TraverseOrder} from "../../dist/binary-search-tree/binary-search-tree";
-
 export type TraverseOrder = 'inOrder' | 'preOrder' | 'postOrder';
 
-export class BinarySearchRoot<T> {
+export class Tree<T> {
 
 }
 
-export class BinarySearchTree<T> {
-    private _left: BinarySearchTree<T> = null;
-    private _right: BinarySearchTree<T> = null;
-    private _condition: (value: T) => void = null;
-    private _compareField: any = null;
+import {TraverseOrder} from "../../../TSDS/src/binary-search-tree/binary-search-tree";
 
-    constructor(public value: T) {}
+export class BinarySearchTree<T> {
+    protected _left: BinarySearchTree<T> = null;
+    protected _right: BinarySearchTree<T> = null;
+
+    constructor(public value: T,
+                public condition?: (value: T) => void ,
+                public compareField?: any) {
+    }
 
     get left(): BinarySearchTree<T> {
         return this._left;
@@ -22,26 +23,74 @@ export class BinarySearchTree<T> {
         return this._right;
     }
 
-    get condition(): (value: T) => void {
-        return this._condition;
-    }
-
-    get compareField(): any {
-        return this._compareField;
-    }
-
     setInsertCondition(cb: (value: T) => void): void {
-        this._compareField = null;
-        this._condition = cb;
+        this.compareField = null;
+        this.condition = cb;
     }
 
     setCompareField(compareField: any) {
         if (compareField !== null && this.value.hasOwnProperty(compareField)) {
-            this._compareField = compareField;
-            this._condition = null;
+            this.compareField = compareField;
+            this.condition = null;
+        }
+    }
+
+    insert(value: T): void {
+        if (value === Object(value) && !this.condition && !this.compareField) {
+            try {
+                throw new Error('To insert non-primitive values, first setInsertCondition or setCompareField');
+            } catch (e) {
+                console.error(e);
+            }
+            return;
+        } else if (value !== Object(value) && !this.condition && !this.compareField) {
+            this.condition = function(val) {
+                return val > this.value;
+            };
+        } else if (!this.condition && this.compareField) {
+            this.condition = function(val) {
+                return val[this.compareField] > this.value[this.compareField];
+            };
+        }
+
+        if (!this.condition(value)) {
+            if (!this._left) {
+                this._left = new BinarySearchTree(value, this.condition, this.compareField);
+            } else {
+                this._left.insert(value);
+            }
+        } else {
+            if (!this._right) {
+                this._right = new BinarySearchTree(value, this.condition, this.compareField);
+            } else {
+                this._right.insert(value);
+            }
+        }
+    }
+
+    traverseDepth(cb: (value: T) => any, order?: TraverseOrder) {
+        if (order && order === 'preOrder') {
+            cb(this.value);
+        }
+        if (this.left) {
+            this._left.traverseDepth(cb, order);
+        }
+        if (!order || order === 'inOrder') {
+            cb(this.value);
+        }
+        if (this.right) {
+            this._right.traverseDepth(cb, order);
+        }
+        if (order && order === 'postOrder') {
+            cb(this.value);
         }
     }
 }
+
+
+
+
+
 
 
 
